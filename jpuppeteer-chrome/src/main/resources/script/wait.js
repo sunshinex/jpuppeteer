70fac6ed9097c87e608c24502155059e81c7e582
jarvis <jarvis.xu@vipshop.com>
2019-12-17 00:22:40 +0800

    +修改wait.js，对于null的返回认为是有效的，只有undefined才会继续轮询

--- jpuppeteer-chrome/src/main/resources/script/wait.js
+++ jpuppeteer-chrome/src/main/resources/script/wait.js
@@ -1,10 +1,11 @@
 async function wait(expression, timeout) {
     return await new Promise(async (resolve, reject) => {
+        const waitInterval = 100;
         let isTimeout = false;
         const args = Array.prototype.slice.call(arguments, 2);
         const func = eval("(" + expression + ")");
         if (!func instanceof Function) {
-            return reject("expression is not a vaild function");
+            return reject(new Error("expression is not a vaild function"));
         }
         async function handler() {
             let result = undefined;
@@ -18,9 +19,9 @@ async function wait(expression, timeout) {
                     //执行超时, 不需要往后执行
                     return;
                 }
-                if (result === null || result === undefined) {
-                    //未取得结果, 需要继续轮询
-                    return setTimeout(handler, 100);
+                if (result === undefined) {
+                    //未返回结果, 需要继续轮询
+                    return setTimeout(handler, waitInterval);
                 }
                 //成功取得轮询结果
                 return resolve(result);
@@ -29,7 +30,7 @@ async function wait(expression, timeout) {
                 return reject(e);
             }
         }
-        setTimeout(handler, 100);
+        setTimeout(handler, waitInterval);
         setTimeout(function(){
             isTimeout = true;
             reject(new Error("timeout"));
