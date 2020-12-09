b9c42580f8db8098f6ed26837aab8614fbfa5a59
jarvis.xu <jarvis.xu@vipshop.com>
2020-12-09 13:57:11 +0800

    修复waitSelector存在文本节点或者注释节点的时候不能返回的BUG

--- src/main/resources/script/waitselector.js
+++ src/main/resources/script/waitselector.js
@@ -1,31 +1,44 @@
 async function waitSelector(selector, timeout) {
+    const root = this === window || this === undefined ? document : this;
     return new Promise(function(resolve, reject){
         const target = document.querySelector(selector);
         if (target != null && target.getClientRects().length > 0) {
             return resolve(target);
         }
+        function checkNode(node) {
+            if (!node.nodeType || node.nodeType != 1 || !node.matches || !(node.matches instanceof Function)) {
+                return;
+            }
+            if (node.matches(selector) && node.getClientRects().length > 0) {
+                observer.disconnect();
+                clearTimeout(timer);
+                resolve(node);
+                return true;
+            }
+        }
         const observer = new MutationObserver(function(mutationsList, observer){
-            for(let mutation of mutationsList) {
-                if (mutation.type === 'childList') {
-                    for (let node of mutation.addedNodes) {
-                        if (node.matches(selector) && node.getClientRects().length > 0) {
-                            observer.disconnect();
-                            clearTimeout(timer);
-                            resolve(mutation.target);
+            try {
+                OUTER:
+                for (let mutation of mutationsList) {
+                    if (mutation.type === 'childList') {
+                        for (let node of mutation.addedNodes) {
+                            if (!!checkNode(node)) {
+                                break OUTER;
+                            }
+                        }
+                    } else if (mutation.type === "attributes") {
+                        if (!!checkNode(mutation.target)) {
+                            break;
                         }
                     }
-                } else if (mutation.type === "attributes") {
-                    if (mutation.target.matches(selector) && mutation.target.getClientRects().length > 0) {
-                        //对象可见
-                        observer.disconnect();
-                        clearTimeout(timer);
-                        resolve(mutation.target);
-                    }
                 }
+            } catch (e) {
+                observer.disconnect();
+                reject(e);
             }
         });
 
-        observer.observe(this === window ? document : this, {
+        observer.observe(root, {
             childList: true,
             subtree: true,
             attributes: true
