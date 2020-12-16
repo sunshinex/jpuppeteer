15143b919233c594cf705b3753dae783b2b73349
jarvis.xu <jarvis.xu@vipshop.com>
2020-12-16 15:11:23 +0800

    修复序列化的时候，枚举是List的时候没法转成value的bug

--- src/main/java/jpuppeteer/util/CDPEnumFilter.java
+++ src/main/java/jpuppeteer/util/CDPEnumFilter.java
@@ -3,12 +3,34 @@ package jpuppeteer.util;
 import com.alibaba.fastjson.serializer.ValueFilter;
 import jpuppeteer.cdp.client.CDPEnum;
 
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 public class CDPEnumFilter implements ValueFilter {
 
+    @SuppressWarnings("unchecked")
     @Override
     public Object process(Object object, String name, Object value) {
         if (value instanceof CDPEnum) {
             return ((CDPEnum) value).value();
+        } else if (value instanceof Collection) {
+            Collection collection = (Collection) value;
+            return collection.stream().map(o -> {
+                if (o instanceof CDPEnum) {
+                    return ((CDPEnum) o).value();
+                } else {
+                    return o;
+                }
+            }).collect(Collectors.toList());
+        } else if (value instanceof Object[]) {
+            Object[] array = (Object[]) value;
+            for(int i=0; i<array.length; i++) {
+                Object o = array[i];
+                if (o instanceof CDPEnum) {
+                    array[i] = ((CDPEnum) o).value();
+                }
+            }
+            return array;
         } else {
             return value;
         }
