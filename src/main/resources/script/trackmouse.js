7cfe2f9a653b2286fff1abd61799f62696d7a5ed
jarvis.xu <jarvis.xu@vipshop.com>
2021-02-02 10:38:02 +0800

    提交trackmouse

--- src/main/resources/script/trackmouse.js
+++ src/main/resources/script/trackmouse.js
@@ -1,15 +1,39 @@
 document.addEventListener('DOMContentLoaded', function(ev){
-    let css = document.createElement('style');
-    css.setAttribute("type", "text/css");
-    css.innerText = "iframe {pointer-events: none;}";
-    document.head.appendChild(css);
-}, false);
+    'use strict';
+    function trackMouse(doc) {
+        doc.addEventListener('mousemove', function(ev){
+            console.log(ev);
+            let node = doc.createElement('div');
+            node.style='width:2px;height:2px;background:#f00;position:fixed;left:'+(ev.clientX+2)+'px;top:'+(ev.clientY+2)+'px;z-index:99999';
+            doc.body.appendChild(node);
+            setTimeout(function(){
+                doc.body.removeChild(node);
+            }, 100);
+        }, false);
 
-document.addEventListener('mousemove', function(ev){
-    let node = document.createElement('div');
-    node.style='width:2px;height:2px;background:#f00;position:fixed;left:'+(ev.clientX+2)+'px;top:'+(ev.clientY+2)+'px;z-index:99999';
-    document.body.appendChild(node);
-    setTimeout(function(){
-        document.body.removeChild(node);
-    }, 100);
+        const observer = new MutationObserver(function(mutationsList, observer){
+            OUTER:
+                for (let mutation of mutationsList) {
+                    if (mutation.type === 'childList') {
+                        for (let node of mutation.addedNodes) {
+                            if (!node || !node.nodeType ||
+                                node.nodeType !== 1 || !node.matches ||
+                                !(node.matches instanceof Function)) {
+                                continue;
+                            }
+                            if (node.nodeName === "IFRAME") {
+                                trackMouse(node.contentDocument);
+                            }
+                        }
+                    }
+                }
+        });
+
+        observer.observe(doc, {
+            childList: true,
+            subtree: true,
+        });
+    }
+
+    trackMouse(document);
 }, false);
